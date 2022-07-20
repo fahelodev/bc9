179f8f791c0b1d98dbf6c1cb016d2a309f8afcf2
Agustin Avila <agustin.avila@tsoftlatam.com>
2022-07-20 12:31:46 -0300

    Commit4

--- src/main/java/aagustin/TenMinWalk.java
+++ src/main/java/aagustin/TenMinWalk.java
@@ -1,18 +1,30 @@
 package aagustin;
 
 public class TenMinWalk {
-    public static boolean isValid(char[] chars) {
-     //   if (chars[]  == 10){
-       //     return true;
-        //} else {return false;}
-        for (int i=0 ; i>chars[] ; i++){
-            if( i == 10){
-                return true;
-            }
-            else {
-                return false;
-            }
+    public static boolean isValid(char[] ruta) {
+    if (!(ruta.length==10)){
+        return false;
+    }
+    int nsPos=0;
+    int ewPos=0;
+    
+    for (int i=0;i<10;i++){
+        char caminar=ruta[i];
+        switch (caminar){
+            case 'n':
+                nsPos++;
+                break;
+            case 's':
+                nsPos--;
+                break;
+            case 'e':
+                ewPos++;
+                break;
+            case 'w':
+                ewPos--;
+                break;
         }
-        //return true;
+    }
+        return (nsPos == 0 && ewPos == 0);
     }
 }
