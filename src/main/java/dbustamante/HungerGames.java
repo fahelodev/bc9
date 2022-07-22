49b3df7f3a8631968a2be00b8d6cdee87f54c3eb
Diego Bustamante <inc.diego.bustamante@gmail.com>
2022-07-22 10:23:11 -0400

    Refactor exercises

--- src/main/java/dbustamante/HungerGames.java
+++ src/main/java/dbustamante/HungerGames.java
@@ -7,68 +7,68 @@ import java.util.List;
 public class  HungerGames {
 
     public static String[] whoEatsWho(final String zoo) {
-        String[] bajada = zoo.split(","); //Convertir String a un Array separado por comas.
-        ArrayList<String> animals= new ArrayList<>(Arrays.asList(bajada)); //Ingresar a ArrayList para uso dinámico (agregar y quitar posiciones)
-        ArrayList<String> resultado= new ArrayList<>();
-        resultado.add(zoo);
+        String[] divideString = zoo.split(","); //Convertir String a un Array separado por comas.
+        ArrayList<String> animals= new ArrayList<>(Arrays.asList(divideString)); //Ingresar a ArrayList para uso dinámico (agregar y quitar posiciones).
+        ArrayList<String> result= new ArrayList<>();
+        result.add(zoo);
         int counter=0; //contador de referencia para escapar de ciclo while
 
-        while (animals.size() > 1 && counter < animals.size()){
-            for (int i = 0; i <= animals.size() - 1; i++) {
+        while (canEat(animals, counter)){
+            for (int i = 0; i <= animals.size() - 1; i++) { //Recorrer Zoo
                 boolean bool;
                 switch (animals.get(i)) {
                     case "bear" -> {
                         List<String> alimento = new ArrayList<>(Arrays.asList("big-fish", "cow", "bug", "chicken", "sheep", "leaves"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "fox" -> {
                         List<String> alimento = new ArrayList<>(Arrays.asList("chicken", "sheep"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "lion" -> {
                         List<String> alimento = new ArrayList<>(Arrays.asList("antelope", "cow"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "panda", "bug", "giraffe" -> {
                         List<String> alimento = new ArrayList<>(List.of("leaves"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "chicken" -> {
                         List<String> alimento = new ArrayList<>(List.of("bug"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "cow", "antelope", "sheep" -> {
                         List<String> alimento = new ArrayList<>(List.of("grass"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "big-fish" -> {
                         List<String> alimento = new ArrayList<>(List.of("little-fish"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
                     }
                     case "leaves", "grass" -> {
                         List<String> alimento = new ArrayList<>(List.of("noeat"));
-                        bool = comer(animals, resultado, i, alimento);
+                        bool = eat(animals, result, i, alimento);
                         if (bool) {
                             counter = 0;
                         }
@@ -84,15 +84,18 @@ public class  HungerGames {
                 if(bool){break;}
             }
         }
-        //animals=wale+fox
-        String result =String.join(",",animals);
-        resultado.add(result);
-        String[] estadoFinalZoo= new String[resultado.size()];
-        estadoFinalZoo=resultado.toArray(estadoFinalZoo);
-        return estadoFinalZoo;
+        String lastLineArray =String.join(",",animals);
+        result.add(lastLineArray);
+        String[] finalStateZoo= new String[result.size()];
+        finalStateZoo=result.toArray(finalStateZoo);
+        return finalStateZoo;
     }
 
-    private static boolean comer(ArrayList<String> animals, ArrayList<String> resultado, int i, List<String> alimento) {
+    private static boolean canEat(ArrayList<String> animals, int counter) {
+        return animals.size() > 1 && counter < animals.size();
+    }
+
+    private static boolean eat(ArrayList<String> animals, ArrayList<String> resultado, int i, List<String> alimento) {
         boolean bool=false;
         if (i-1 >= 0 && alimento.contains(animals.get(i - 1))) {
             resultado.add(animals.get(i) + " eats " + animals.get(i - 1));
