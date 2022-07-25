b21ea566f58c75ed2b1d1a6e4e421f8456f9b763
Brandon Muñoz <brandon.munoz@tsoftglobal.com>
2022-07-25 09:23:34 -0400

    Ultimos Ejercicios en Equipo

--- src/main/java/bMunoz/KataFight.java
+++ src/main/java/bMunoz/KataFight.java
@@ -4,34 +4,52 @@ public class KataFight {
 
     public static String declareWinner(KataFighter fighter1, KataFighter fighter2, String firstAttacker) {
         String winner = "";
-        Boolean aux = false;
-        while (aux==false) {
-            if (firstAttacker == fighter1.name) {
+        boolean aux = false;
+
+        while (HayGanador(aux))
+        {
+            if (GolpeaJugador1(fighter1, firstAttacker))
+            {
                 fighter2.health -= fighter1.damagePerAttack;
-                if (fighter2.health <= 0) {
+
+                if (SinVidaJugador2(fighter2))
+                {
+                    aux = true;
                     winner= fighter1.name;
-                    aux = true;
-                } else {
-                    fighter1.health -= fighter2.damagePerAttack;
-                    if (fighter1.health <= 0) {
-                        winner= fighter2.name;
-                        aux = true;
-                    }
+
                 }
-            } else if(firstAttacker == fighter2.name){
+                firstAttacker = fighter2.name;
+            }
+            else if(GolpeaJugador2(fighter2, firstAttacker))
+            {
                 fighter1.health -= fighter2.damagePerAttack;
-                if (fighter1.health <= 0) {
-                    winner= fighter2.name;
+
+                if (SinVidaJugador1(fighter1))
+                {
                     aux = true;
-                } else {
-                    fighter2.health -= fighter1.damagePerAttack;
-                    if (fighter2.health <= 0) {
-                        winner= fighter1.name;
-                        aux=true;
-                    }
+                    winner= fighter2.name;
                 }
+                firstAttacker = fighter1.name;
             }
         }
         return winner;
+    }
+
+    private static boolean SinVidaJugador2(KataFighter fighter2) {
+        return fighter2.health <= 0;
+    }
+    private static boolean SinVidaJugador1(KataFighter fighter1) {
+        return fighter1.health <= 0;
+    }
+    private static boolean GolpeaJugador1(KataFighter fighter1, String firstAttacker) {
+        return firstAttacker.equals(fighter1.name);
+    }
+
+    private static boolean GolpeaJugador2(KataFighter fighter2, String firstAttacker) {
+        return firstAttacker.equals(fighter2.name);
+    }
+
+    private static boolean HayGanador(Boolean aux) {
+        return !aux;
     }
 }
