c726cf456159f80954872f5d66bd13eba3879a59
Juan Jesus Flores <jesus.flores@tsoftglobal.com>
2022-08-15 15:23:26 -0300

    caso 001 tren

--- src/test/java/desafio/grupo2/rumbo/testcreation/pages/RumboEsTrenesPage.java
+++ src/test/java/desafio/grupo2/rumbo/testcreation/pages/RumboEsTrenesPage.java
@@ -10,11 +10,38 @@ public class RumboEsTrenesPage extends SeleniumWrapper {
         super(driver);
     }
 
-    By SoloIda = By.xpath( "//div[@class='trainSwitch trainOneWay active']");
-    By OrigenTren = By.xpath( "//div[text()='Madrid']");
-    By DestinoTren = By.xpath("//div[text()='Barcelona']");
-    By fechaTren =By.xpath("//div[@class=\"calendarBoxText lmn-sw-responsive-form-field lmn-sw-tooltip-responsive__text\"][text()=\"Mié, 24 Ago\"]");
-    By TurnoTren = By.xpath("//div[@class=\"timeSelectionBoxTitle\"][text()='Tarde']");
+    By btnsoloIda = By.xpath( "//div[@class='trainSwitch trainOneWay active']");
+    By btnorigenTren = By.xpath( "//div[@class=\"select-selected focus\"]");
+
+    By btnDestinoTren = By.xpath("//div[@class=\"select-selected focus\"][1]");
+    By btnCalendario = By.xpath("//div[@class=\"calendarBox lmn-sw-select-responsive lmn-sw-tooltip-responsive lmn-sw-tooltip-responsive__active\"]");
+
+
     By BotonBuscar = By.xpath("//div[@class=\"btn btn-cta btn-block lmn-sw-submitFlight\"]");
+
+    public void seleccionarIda(){
+        click(btnsoloIda);
+    }
+    public void ingresarOrigen(){
+        click(btnorigenTren);
+        By origenmadrid =By.xpath("//div[@data-value=\"MAD\"][@class=\"same-as-selected\"]");
+        click(origenmadrid);
+    }
+    public void ingresarDestino(){
+        click(btnorigenTren);
+        By destinoBarcelona =By.xpath("//div[@data-value=\"YJB\"][@class=\"same-as-selected\"]");
+        click(destinoBarcelona);
+    }
+    public void seleccionarCalendario(){
+        click(btnCalendario);
+        By turnoTren = By.xpath("//div[@class=\"timeSelectionBoxTitle\"][text()='Tarde']");
+        click(turnoTren);
+        By fechaTren =By.xpath("//div[@class=\"calendarBoxText lmn-sw-responsive-form-field lmn-sw-tooltip-responsive__text\"][text()=\"Mié, 24 Ago\"]");
+        click(fechaTren);
+    }
+    public void establecerBusqueda(){
+        click(BotonBuscar);
+
+    }
 
 }
--- src/test/java/desafio/grupo2/rumbo/testcreation/testcases/CP002_Trenes.java
+++
@@ -1,6 +0,0 @@
-package desafio.grupo2.rumbo.testcreation.testcases;
-
-import framework.engine.selenium.SeleniumTestBase;
-
-public class CP002_Trenes extends SeleniumTestBase {
-}
