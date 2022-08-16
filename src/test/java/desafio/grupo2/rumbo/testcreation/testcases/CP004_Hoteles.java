4168016936cd65deb48e751ab0b66ebeee8a385f
Matimillan <millansoliscristian@gmail.com>
2022-08-15 20:28:50 -0400

    Commit dia 15/08 sesion 2 avance bases y ultimas cordinaciones para trabajar

---
+++ src/test/java/desafio/grupo2/rumbo/testcreation/testcases/CP004_Hoteles.java
@@ -0,0 +1,32 @@
+package desafio.grupo2.rumbo.testcreation.testcases;
+
+import desafio.grupo2.rumbo.testcreation.pages.RumboEsHomePage;
+import desafio.grupo2.rumbo.testcreation.pages.RumboEsHotelesPage;
+import framework.engine.selenium.DriverFactory;
+import framework.engine.selenium.SeleniumTestBase;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CP004_Hoteles extends SeleniumTestBase {
+
+    RumboEsHomePage rumboEsHomePage;
+
+    RumboEsHotelesPage rumboEsHotelesPage;
+
+    @Test
+    @Description("Test Caso CP004_Hoteles")
+    void CP004_Hoteles() throws InterruptedException {
+        rumboEsHomePage = new RumboEsHomePage(DriverFactory.getDriver());
+        rumboEsHomePage.despegarARumbos();
+        rumboEsHomePage.aceptarCookies();
+        rumboEsHomePage.irAHoteles();
+
+        Assertions.assertEquals("Hoteles | Ofertas de Hoteles baratos | Rumbo", rumboEsHomePage.getUrlTitle());
+
+        rumboEsHotelesPage = new RumboEsHotelesPage(DriverFactory.getDriver());
+        rumboEsHotelesPage.ingresarDestino("Chicago");
+        rumboEsHotelesPage.establecerFechaViaje();
+    }
+}
+
