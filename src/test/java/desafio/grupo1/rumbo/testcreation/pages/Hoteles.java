1c24c8c8cd1509240244b92653b9b6216cda7fdb
Andrea Holland <andrea.holland@tsoftglobal.com>
2022-08-17 14:18:49 -0300

    Agregado de Xpaths necesarios para las pruebas

--- src/test/java/desafio/grupo1/rumbo/testcreation/pages/Hoteles.java
+++ src/test/java/desafio/grupo1/rumbo/testcreation/pages/Hoteles.java
@@ -1,4 +1,34 @@
 package desafio.grupo1.rumbo.testcreation.pages;
 
-public class Hoteles {
+import framework.engine.selenium.SeleniumWrapper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Hoteles extends SeleniumWrapper {
+
+    public Hoteles(WebDriver driver) {
+        super(driver);
+    }
+
+    //----------------LOCALIZADORES PRUEBA ID_H05
+    By txtAreaADondeQuieresIr = By.xpath("//*[@id=\"mui-1\"]");
+    By calendarFechaIda = By.xpath("//button[@class=\"display-5j7qlk-DateInput-styled\"]");
+    By seleccionFechaIda = By.xpath ("(//button[text()='28'])[1]");
+    By seleccionFechaVuelta = By.xpath("//button[text()='31']");
+    By btnBuscar = By.xpath("//div/button[@type=\"submit\"]");
+
+    By opcionHotelHardRock = By.xpath("descendant::div[text()=\"Hard Rock Hotel Cancun All-Inclusive\"]");
+    By btnElegirTipoDeHabitacion = By.xpath("//button[@data-test-id=\"book-cta\"]");
+    By btnTipoHabContinuar = By.xpath("(//button[@data-testid=\"checkout-button\"])[1]");
+    By seccionDatosHotel = By.xpath("descendant::div[@class=\"hotel-summary__inner\"]");
+
+
+    //----------------LOCALIZADORES PRUEBA ID_H06
+
+    By btnHoteles = By.xpath("(//li/div/a[@title=\"Hoteles\"])[1]");
+    By desplegarIdeasHotelesEspania = By.xpath ("(//*[@id=\"arrow_down_svg__filled\"])[1]");
+    By desplegarDestPopularesExtanj = By.xpath("(//*[@id=\"arrow_down_svg__filled\"])[2]");
+    By desplegarHtlesCiudadesEspa = By.xpath ("(//*[@id=\"arrow_down_svg__filled\"])[3]");
+    By desplegarIdeasDeViajes = By.xpath("(//*[@id=\"arrow_down_svg__filled\"])[4]");
+
 }
