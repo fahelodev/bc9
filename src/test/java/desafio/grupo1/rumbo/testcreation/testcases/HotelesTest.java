48cdafd19b1a625589c7713ab592a42b685a1784
Matias <matias.zalazar@tsoftlatam.com>
2022-08-16 21:02:18 -0300

    Modificado cada Test con BeforEach para inicializar driver, ingresar a pagina web y limpiar cookies

--- src/test/java/desafio/grupo1/rumbo/testcreation/testcases/HotelesTest.java
+++ src/test/java/desafio/grupo1/rumbo/testcreation/testcases/HotelesTest.java
@@ -1,4 +1,17 @@
 package desafio.grupo1.rumbo.testcreation.testcases;
 
-public class HotelesTest {
+import desafio.grupo1.rumbo.testcreation.pages.RumboHomePage;
+import framework.engine.selenium.DriverFactory;
+import framework.engine.selenium.SeleniumTestBase;
+import org.junit.jupiter.api.BeforeEach;
+
+public class HotelesTest extends SeleniumTestBase {
+    RumboHomePage rumboHomePage;
+    @BeforeEach
+    public void inicializar(){
+        rumboHomePage = new RumboHomePage(DriverFactory.getDriver());
+        rumboHomePage.navegarAlHome();
+        rumboHomePage.aceptarCookies();
+    }
+
 }
--- src/test/java/desafio/grupo1/rumbo/testcreation/testcases/TrenesTest.java
+++ src/test/java/desafio/grupo1/rumbo/testcreation/testcases/TrenesTest.java
@@ -5,17 +5,21 @@ import desafio.grupo1.rumbo.testcreation.pages.RumboHomePage;
 
 import framework.engine.selenium.DriverFactory;
 import framework.engine.selenium.SeleniumTestBase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TrenesTest extends SeleniumTestBase {
 
     RumboHomePage rumboHomePage;
-
-    @Test
-    public void irASeccionTrenes() {
+    @BeforeEach
+    public void inicializar(){
         rumboHomePage = new RumboHomePage(DriverFactory.getDriver());
         rumboHomePage.navegarAlHome();
         rumboHomePage.aceptarCookies();
+    }
+    @Test
+    public void irASeccionTrenes() {
+
         rumboHomePage.seccionTrenes();
     }
 
