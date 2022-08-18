e84e7f37a5c5081abc90c5b88396435657148567
Agustin Avila <agustin.avila@tsoftlatam.com>
2022-08-18 18:23:13 -0300

    Test 1 al 4 finalizados en HotelesTest y metodos agregados en Hoteles.

--- src/test/java/desafio/grupo1/rumbo/testcreation/pages/Hoteles.java
+++ src/test/java/desafio/grupo1/rumbo/testcreation/pages/Hoteles.java
@@ -12,15 +12,27 @@ public class Hoteles extends SeleniumWrapper {
     public Hoteles(WebDriver driver) {
         super(driver);
     }
+    By btnAceptarCokies = By.xpath("//button[@data-qa='oil-YesButton']");  /*btn cook hacer click*/
     //----------------LOCALIZADORES PRUEBA ID_H01
     By btnDestino = By.xpath("//*[@id=\"mui-1\"]");
     By btnCualquierDestino= By.xpath("//*[@id=\"mui-1-option-0\"]");
-    By btnHabitaciones= By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[3]/div[1]/div/div[1]");
-    By agregarHabitaciones= By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[3]/div[1]/div/div[2]/div/div[2]/button");
-    By filtroHabitacion= By.xpath("//*[@id=\"__next\"]/header/div[1]/div[3]/div[6]/span");
-    //----------------LOCALIZADORES PRUEBA ID_H02
+    By btnHabitaciones= By.xpath("//div[@class='display-x5p9rc-Dropdown-styled']");
+    By agregarHabitaciones= By.xpath("//button[@class='display-iohcwh-ParticipantsDropdown-styled'] ");
+    By filtroHabitacion= By.xpath("//div[@class='styled__PlaceholderStyled-sc-hqc7hu-0 buZeto openx-ui-header-collapsed-placeholder-people']");
 
-    By btnAceptarCokies = By.xpath("//button[@data-qa='oil-YesButton']");  /*btn cook hacer click*/
+    //----------------LOCALIZADORES PRUEBA ID_H02---------------
+    By btnFiltroEstrellas= By.xpath("(//div[@class='styled__CustomCompStyledLabel-sc-1v205vs-1 izWgpy custom-radio-check'])[2]");
+    By btn4Estrellas = By.xpath("(//div[@class='styled__CustomOverlayItem-sc-1prstuv-0 kiZdSH'])[2]");
+    By btnBuscarEstrellas = By.xpath("//button[contains(text(),'Aplicar')] ");
+    By filtroEstrellas = By.xpath("//div[@class='styled__StarsLabel-sc-mcln4k-0 eVomHt']");
+    //----------------LOCALIZADORES PRUEBA ID_H03---------------
+    By btnFiltroPrecio = By.xpath("//div[@class='styled__CustomCompStyledLabel-sc-1v205vs-1 izWgpy openx-ui-price-and-range']");
+    By btn0a300 = By.xpath("(//div[@class='styled__RangesContainerItem-sc-vm35j3-2 eKBkSD'])[3]");
+    By btnAplicarPrecio = By.xpath("//button[@class='styled__ApplyStyled-sc-18vcrz8-4 cHQYkF']");
+    By filtroRango = By.xpath("//div[@class='styled__CustomCompStyledLabel-sc-1v205vs-1 dIGINx openx-ui-price-and-range']");
+    //----------------LOCALIZADORES PRUEBA ID_H04
+    By btnHotelesRurales = By.xpath("//div[@class='display-1aslju6-HubFeatureCard-styled e1apqt34'] ");
+    By textoHotelesRurales= By.xpath("(//div/h1)[1]");
 
     //----------------LOCALIZADORES PRUEBA ID_H05
     By txtAreaADondeQuieresIr = By.xpath("//*[@id=\"mui-1\"]");
@@ -42,7 +54,7 @@ public class Hoteles extends SeleniumWrapper {
     By desplegarHtlesCiudadesEspa = By.xpath ("(//*[@id=\"arrow_down_svg__filled\"])[3]");
     By desplegarIdeasDeViajes = By.xpath("(//*[@id=\"arrow_down_svg__filled\"])[4]");
 
-    //---------------------METODOS
+    //---------------------METODOS---------------------
 
     public void navegarAlHome(){
         navigateTo(BASE_URL_AUT);
@@ -54,19 +66,49 @@ public class Hoteles extends SeleniumWrapper {
     //---------------------METODOS----------------------
     public void seccionHoteles(){ click(btnHoteles);}
     public String busquedaHabitacionPorPersona(){
-        String filtro;
+        //String filtro;
         click(btnDestino);
-        if (isDisplayed(btnCualquierDestino)){
-        }
-        if (isDisplayed(btnHabitaciones)){click(btnHabitaciones);}
-            if (isDisplayed(agregarHabitaciones)){
-                click(agregarHabitaciones);
-            }
-        if (isDisplayed(btnBuscar)){
-            click(btnBuscar);
-        }
-        filtro = getText(filtroHabitacion);
+        esperaEnSegundosYClick(10,btnCualquierDestino);
+        esperaEnSegundosYClick(10,btnHabitaciones);
+        esperaEnSegundosYClick(10,agregarHabitaciones);
+        esperaEnSegundosYClick(10,btnBuscar);
+        String filtro = getText(filtroHabitacion);
         return filtro;
+    }
+    public String busquedaFiltroEstrellas() throws InterruptedException {
+        click(btnDestino);
+        Thread.sleep(5000);
+        click(btnBuscar);
+        Thread.sleep(6000);
+        click(btnFiltroEstrellas);
+        Thread.sleep(7000);
+        click(btn4Estrellas);
+        Thread.sleep(5000);
+        click(btnBuscarEstrellas);
+        String filtroEstrella = getText(filtroEstrellas);
+        return filtroEstrella;
+    }
+    public String busquedaFiltroRangoDePrecio() throws InterruptedException {
+    click(btnDestino);
+    Thread.sleep(3000);
+    click(btnBuscar);
+    Thread.sleep(4000);
+    click(btnFiltroPrecio);
+    Thread.sleep(3000);
+    click(btn0a300);
+    click(btnAplicarPrecio);
+    Thread.sleep(4000);
+    String filtroRangoPrecio = getText(filtroRango);
+    return filtroRangoPrecio;
+    }
+    public void tituloHoteleresRurales(){
+    click(btnHotelesRurales);
+    }
+    public String getHoteles(){
+        String checkeo =getText(textoHotelesRurales);
+        System.out.println(checkeo);
+        return  checkeo;
+
     }
 
 
--- src/test/java/desafio/grupo1/rumbo/testcreation/testcases/HotelesTest.java
+++ src/test/java/desafio/grupo1/rumbo/testcreation/testcases/HotelesTest.java
@@ -3,27 +3,45 @@ package desafio.grupo1.rumbo.testcreation.testcases;
 import desafio.grupo1.rumbo.testcreation.pages.Hoteles;
 import framework.engine.selenium.DriverFactory;
 import framework.engine.selenium.SeleniumTestBase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class HotelesTest extends SeleniumTestBase {
-    Hoteles H01;
+    Hoteles hoteles;
 
     @BeforeEach
     public void inicializar() {
-        H01= new Hoteles(DriverFactory.getDriver());
-        H01.navegarAlHome();
-        H01.aceptarCookies();
+        hoteles = new Hoteles(DriverFactory.getDriver());
+        hoteles.navegarAlHome();
+        hoteles.aceptarCookies();
     }
 
 
     @Test
     public void Test_H01() {
+        hoteles.seccionHoteles();
+        Assertions.assertEquals("3 viajeros, 1 habitación", hoteles.busquedaHabitacionPorPersona());
+    }
+    @Test
+    public void Test_H02() throws InterruptedException {
+        hoteles.seccionHoteles();
+        Assertions.assertEquals("4 estrellas",hoteles.busquedaFiltroEstrellas());
+    }
 
-        H01.seccionHoteles();
-        H01.busquedaHabitacionPorPersona();
-
+    @Test
+    public void Test_H03() throws InterruptedException {
+        hoteles.seccionHoteles();
+        Assertions.assertEquals("Hasta 300 €",hoteles.busquedaFiltroRangoDePrecio());
+    }
+    @Test
+    public void Test_H04() {
+        hoteles.seccionHoteles();
+        hoteles.tituloHoteleresRurales();
+        hoteles.guardarPag();
+        hoteles.switchTo(1);
+        Assertions.assertEquals("Hoteles Rurales",hoteles.getHoteles());
     }
 
 }
